1c79c7caed160fdc52b5221458820c8484183755
BLACKI-J <73060617+BLACKI-J@users.noreply.github.com>
2024-07-12 11:19:58 +0200

    Add files via upload

---
+++ 3-ebp.bat
@@ -0,0 +1,4 @@
+@echo
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+powershell.exe -executionpolicy bypass -file "%~dp0\install-EBP-3.ps1"
